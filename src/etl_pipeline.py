dbb8f3fd64e3daed5c32ead4f1a48c2bac9f6eb7
pipipuppypaul <bobbywdiphone6@gmail.com>
2023-06-15 06:26:06 -0700

    [mod] data types handled in ETL

--- src/etl_pipeline.py
+++ src/etl_pipeline.py
@@ -1,13 +1,20 @@
-import pandas as pd
 import argparse
-import os
 import logging
+import os
+
+import pandas as pd
+
+from config.configs import db_data_column_mapping
+
 logging.basicConfig(level=logging.INFO)
 
 class ETLPipeLine:
     def __init__(self, input_file, output_file):
+        if not os.path.exists(input_file):
+            raise Exception('Input file does not exist')
         self.input_file = input_file
         self.output_file = output_file
+        self.df = None
 
     def read_data(self):
         # file: /Users/yuhaibo/Downloads/combined_transactions.csv
@@ -15,7 +22,12 @@ class ETLPipeLine:
         logging.info('Data read from: {}'.format(self.input_file))
 
     def transform_data(self):
-        self.df = self.df.sort_values(by=['datetime'])
+        assert len(db_data_column_mapping['columns']) == len(db_data_column_mapping['types'])
+        for i in range(len(db_data_column_mapping['columns'])):
+            col = db_data_column_mapping['columns'][i]
+            tp = db_data_column_mapping['types'][i]
+            self.df[col] = self.df[col].astype(tp)
+        self.df = self.df.sort_values(by=['datetime']) #used for future fast fetch
 
     def write_data(self):
         self.df.to_parquet(self.output_file, index=False, partition_cols=['transaction_type'])
