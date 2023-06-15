70eee9b0b3b2d75d29306db9a2104644bfc08939
pipipuppypaul <bobbywdiphone6@gmail.com>
2023-06-15 06:34:56 -0700

    [refactor] Code refactor

--- server.py
+++ server.py
@@ -27,9 +27,9 @@ def get_transaction(userId):
                                columns=server_data_column_mapping.get("transaction").get("input_col"),)
     dataset = cache.trans_data.copy()
     dataset = dataset[dataset['user_id'] == int(userId)]
-    dataset.loc[:, 'amount_in_dollars'] = round(dataset.loc[:, 'amount_cents'] / 100, 0)
-    dataset.loc[:, 'amount_in_dollars'] = dataset.loc[:,'amount_in_dollars'].astype(int)
-    dataset.loc[:, 'datetime'] = dataset['datetime'].dt.strftime('%Y-%m-%dT%H:%M:%S+00:00')
+    dataset['amount_in_dollars'] = round(dataset.loc[:, 'amount_cents'] / 100, 0)
+    dataset['amount_in_dollars'] = dataset.loc[:,'amount_in_dollars'].astype(int)
+    dataset['datetime'] = dataset.loc[:, 'datetime'].dt.strftime('%Y-%m-%dT%H:%M:%S+00:00')
     result_data = dataset.loc[:, server_data_column_mapping.get("transaction").get("output_col")]
     logging.info("{} records returned".format(result_data.shape[0]))
     return result_data.to_json(orient='records')
@@ -44,17 +44,17 @@ def get_net_return(merchantTypeCode):
                                 columns=server_data_column_mapping.get("net_return").get("input_col"),)
     all_data = cache.net_amt_data.copy()
     all_data = all_data[all_data['merchant_type_code'] == int(merchantTypeCode)]
-    all_data.loc[:, 'date'] = all_data.loc[:, 'datetime'].dt.strftime('%Y-%m-%d')
+    all_data['date'] = all_data.loc[:, 'datetime'].dt.strftime('%Y-%m-%d')
     purchase_data = all_data[all_data['transaction_type'] == 'PurchaseActivity']
     return_data = all_data[all_data['transaction_type'] == 'ReturnActivity']
     purchase_data = purchase_data.groupby(['date']).agg({'amount_cents': 'sum'}).reset_index()
     return_data = return_data.groupby(['date']).agg({'amount_cents': 'sum'}).reset_index()
     result_data = pd.merge(purchase_data, return_data, on='date', how='outer', suffixes=('_purchase', '_return'))
-    result_data.loc[:, 'amount_cents_purchase'] = result_data.loc[:, 'amount_cents_purchase'].fillna(0)
-    result_data.loc[:, 'amount_cents_return'] = result_data.loc[:, 'amount_cents_return'].fillna(0)
-    result_data.loc[:, 'net_amount_in_dollars'] = round((result_data.loc[:, 'amount_cents_purchase'] - result_data['amount_cents_return']) / 100, 0)
-    result_data.loc[:, 'net_amount_in_dollars'] = result_data.loc[:, 'net_amount_in_dollars'].astype(int)
-    result_data.loc[:, 'merchant_type_code'] = int(merchantTypeCode)
+    result_data['amount_cents_purchase'] = result_data.loc[:, 'amount_cents_purchase'].fillna(0)
+    result_data['amount_cents_return'] = result_data.loc[:, 'amount_cents_return'].fillna(0)
+    result_data['net_amount_in_dollars'] = round((result_data.loc[:, 'amount_cents_purchase'] - result_data['amount_cents_return']) / 100, 0)
+    result_data['net_amount_in_dollars'] = result_data['net_amount_in_dollars'].astype(int)
+    result_data['merchant_type_code'] = int(merchantTypeCode)
     result_data = result_data.loc[:, server_data_column_mapping.get("net_return").get("output_col")]
     logging.info("{} records returned".format(result_data.shape[0]))
     return result_data.to_json(orient='records')
